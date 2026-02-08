12f18babd1ee6a47a165d7d3eb9473d90a2e5d53
John Ivan Chan <chanjohn@oregonstate.edu>
2026-02-08 14:23:33 -0800

    finished valentines project!

---
+++ js/yes-script.js
@@ -0,0 +1,4 @@
+ghostImage = document.getElementById("ghostImage");
+ghostImage.addEventListener("click", function() {
+    location.href = "https://www.uwufufu.com/worldcup/valentines-date-2026-valentines-date-2026";
+});
